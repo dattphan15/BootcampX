ac4ab9bf8cf54651bcdb024d29719fb9b32ed59b
Kevin Phan <ken.phan@live.ca>
2021-03-02 01:14:12 +0000

    Add assignment & assignment_submission entities

--- 1_queries/1_students_names_from_cohort.sql
+++ 1_queries/1_students_names_from_cohort.sql
@@ -1,35 +1,36 @@
--- SELECT * FROM students
+-- Show full table
+SELECT * FROM students
 
 -- Get the names of all of the students from a single cohort.
--- SELECT id, name
---   FROM students
---   WHERE cohort_id = '1'
---   ORDER BY name;
+SELECT id, name
+  FROM students
+  WHERE cohort_id = '1'
+  ORDER BY name;
 
 
 -- Select the total number of students who were in the first 3 cohorts.
--- SELECT COUNT(id)
---   FROM students
---   WHERE cohort_id IN (1,2,3);
+SELECT COUNT(id)
+  FROM students
+  WHERE cohort_id IN (1,2,3);
 
 
 -- Get all of the students that don't have an email or phone number.
-  -- SELECT name, id, cohort_id
-  --   FROM students
-  --   WHERE email IS NULL OR phone IS NULL;
+  SELECT name, id, cohort_id
+    FROM students
+    WHERE email IS NULL OR phone IS NULL;
 
 
 -- Get all of the students without a gmail.com or phone number.
-  -- SELECT name, email, id, cohort_id
-  --   FROM students
-  --   WHERE email NOT LIKE '%gmail.com' AND phone IS NULL;
+  SELECT name, email, id, cohort_id
+    FROM students
+    WHERE email NOT LIKE '%gmail.com' AND phone IS NULL;
 
 
 -- Get all of the students currently enrolled. A student's end date will be NULL when they are currently enrolled in Bootcamp.
-  -- SELECT name, id, cohort_id
-  --   FROM students
-  --   WHERE end_date IS NULL
-  --   ORDER BY cohort_id;
+  SELECT name, id, cohort_id
+    FROM students
+    WHERE end_date IS NULL
+    ORDER BY cohort_id;
 
 
 -- Once a student graduates, the career services department needs to keep track of their activity on Github. If a graduate has not linked their Github account, we need to contact them to do so. Get all graduates without a linked Github account.
