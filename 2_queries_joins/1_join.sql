613e4dd925c4aa718a6c8a371c077e91d9b4fbb4
Kevin Phan <ken.phan@live.ca>
2021-03-02 03:37:35 +0000

    2_queries_join

--- 2_queries_joins/1_join.sql
+++ 2_queries_joins/1_join.sql
@@ -1,9 +1,9 @@
 -- Get the total amount of time that a student has spent on all assignments. This should work for any student but use 'Ibrahim Schimmel' for now. Select only the total amount of time as a single column.
 
--- SELECT SUM(assignment_submissions.duration) as total_duration
---   FROM assignment_submissions
---   JOIN students ON student_id = students.id
---   WHERE students.name = 'Ibrahim Schimmel';
+SELECT SUM(assignment_submissions.duration) as total_duration
+  FROM assignment_submissions
+  JOIN students ON student_id = students.id
+  WHERE students.name = 'Ibrahim Schimmel';
 
 
 -- Get the total amount of time that all students from a specific cohort have spent on all assignments. This should work for any cohort but use FEB12 for now. Select only the total amount of time as a single column. You can write as many JOIN statements as you need in a single query.
