3941af4dd231b890b8c2ded3566f54d69aa2ab14
Kevin Phan <ken.phan@live.ca>
2021-03-02 23:22:51 +0000

    added fake data t assistance request and teachers table

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,6 +1,6 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAT(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   is_active BOOLEAN DEFAULT TRUE,
   start_date DATE,
   end_date DATE
